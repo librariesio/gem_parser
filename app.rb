89152c3940a4041488ba705c16ad8fb004b2a8e3
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-09-07 13:34:22 +0100

    Use form encoded data instead

--- app.rb
+++ app.rb
@@ -7,18 +7,19 @@ require_relative 'lib/gem_parser'
 class RubyDepParser < Sinatra::Base
   use Rack::Deflater
 
-  before do
-    request.body.rewind
-    @request_payload = request.body.read
+  configure do
+    set :dump_errors, false
+    set :raise_errors, true
+    set :show_exceptions, false
   end
 
   post '/gemspec' do
     content_type :json
-    GemParser.new(:gemspec, @request_payload).to_json
+    GemParser.new(:gemspec, params[:body]).to_json
   end
 
   post '/gemfile' do
     content_type :json
-    GemParser.new(:gemfile, @request_payload).to_json
+    GemParser.new(:gemfile, params[:body]).to_json
   end
 end
